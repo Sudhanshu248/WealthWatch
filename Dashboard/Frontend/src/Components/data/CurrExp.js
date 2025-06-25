1b3413236dcd285b820e79cdce6844ecb3eaf27c
Himanshu-kumar025 <hk1832141@gmail.com>
2025-06-25 13:51:18 +0530

    navigate to dashboard

---
+++ Dashboard/Frontend/src/Components/data/CurrExp.js
@@ -0,0 +1,70 @@
+export const CurrExpence = {
+    totalBudget: {
+        value: 1000,
+    },
+
+    expence: {
+        food: {
+            daal: {
+                value: 50
+            },
+
+            samosa: {
+                value: 40
+            },
+            coldDrink: {
+                value: 30
+            },
+            milk: {
+                value: 30
+            },
+            biscuit: {
+                value: 50
+            },
+        },
+
+        transport: {
+            petrol: {
+                value: 100
+            },
+            bus: {
+                value: 30
+            },
+            auto: {
+                value: 20
+            },
+        },
+
+        personalExp: {
+            clothes: {
+                value: 100
+            },
+            grooming: {
+                value: 50
+            },
+        },
+
+        housing: {
+            rent: {
+                value: 200
+            },
+            electricity: {
+                value: 60
+            },
+            water: {
+                value: 20
+            },
+        },
+
+        saving: {
+            bank: {
+                value: 100
+            },
+            cash: {
+                value: 70
+            },
+        },
+    },
+};
+
+
---
+++ Dashboard/Frontend/src/Components/data/lastMonthExp.js
@@ -0,0 +1,69 @@
+
+export const lastMonthExp = {
+    totalBudget: {
+        value: 1200,
+    },
+
+    expence: {
+        food: {
+            daal: {
+                value: 60
+            },
+
+            samosa: {
+                value: 45
+            },
+            coldDrink: {
+                value: 35
+            },
+            milk: {
+                value: 40
+            },
+            biscuit: {
+                value: 55
+            },
+        },
+
+        transport: {
+            petrol: {
+                value: 110
+            },
+            bus: {
+                value: 35
+            },
+            auto: {
+                value: 25
+            },
+        },
+
+        personalExp: {
+            clothes: {
+                value: 120
+            },
+            grooming: {
+                value: 60
+            },
+        },
+
+        housing: {
+            rent: {
+                value: 220
+            },
+            electricity: {
+                value: 70
+            },
+            water: {
+                value: 25
+            },
+        },
+
+        saving: {
+            bank: {
+                value: 130
+            },
+            cash: {
+                value: 80
+            },
+        },
+    }
+};
