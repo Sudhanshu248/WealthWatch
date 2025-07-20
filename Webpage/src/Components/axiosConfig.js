b9a7dafec2db5a52c9c29efc9b90328ae1ecaae6
sudhanshu248 <sainisudhanshu389@gmail.com>
2025-07-21 00:26:22 +0530

    added live links2

--- Webpage/src/Components/axiosConfig.js
+++ Webpage/src/Components/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // Define base URLs for different parts of the application
-export const BASE_URL = "https://wealthwatch-backend-ecg7.onrender.com/";        // Backend server
-export const WEBPAGE_URL = "https://wealth-watch-sandy.vercel.app/";     // Frontend public-facing webpage
-export const DASHBOARD_URL = "https://wealth-watch-dashboard.vercel.app/";   // Frontend admin/user dashboard
+export const BASE_URL = "https://wealthwatch-backend-ecg7.onrender.com";        // Backend server
+export const WEBPAGE_URL = "https://wealth-watch-sandy.vercel.app";     // Frontend public-facing webpage
+export const DASHBOARD_URL = "https://wealth-watch-dashboard.vercel.app";   // Frontend admin/user dashboard
 
 // Create an Axios instance pre-configured with base settings
 const clientServer = axios.create({
